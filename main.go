ab884a0728d60b4dbefdbb358f95d4147eacd340
xLanStar <danny95624268@gmail.com>
2026-09-07 00:37:59 +0800

    chore: bump version to v0.8.0

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ const (
 	exitServer    = 3
 )
 
-var version = "0.7.0"
+var version = "0.8.0"
 
 func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
